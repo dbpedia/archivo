edfbe5dcb2f6805b8c0698f2084b674e078a10b7
Denis Streitmatter <streitmatter@informatik.uni-leipzig.de>
2020-06-18 16:37:46 +0200

    Removes debugging lines

--- archivo/utils/inspectVocabs.py
+++ archivo/utils/inspectVocabs.py
@@ -324,9 +324,3 @@ def checkShaclReport(shaclReportGraph):
         return "Warning"
     else:
         return "OK"
-
-graph = getGraphOfVocabFile("/home/denis/Workspace/Job/Archivo/testdir/purl.org/NET--biol--ns/2020.06.18-153052/NET--biol--ns_type=parsed.ttl")
-
-label = getLabel(graph)
-
-print(label)
