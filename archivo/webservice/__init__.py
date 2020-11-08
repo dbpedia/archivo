59aece6c0a9a534da0c158514c87c3cd97a6046f
Denis Streitmatter <streitmatter@informatik.uni-leipzig.de>
2020-11-08 14:29:21 +0100

    Some more bugfixes

--- archivo/webservice/__init__.py
+++ archivo/webservice/__init__.py
@@ -2,12 +2,9 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from webservice.config import Config
-from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
-
-Bootstrap(app=app)
 
 from webservice import routes, dbModels
