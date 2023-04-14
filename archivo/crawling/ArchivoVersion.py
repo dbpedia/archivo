d39c0c97a7e8a30f180a1faa447786db3e48f896
Denis <yum-yab-dev@pm.me>
2023-04-14 15:12:13 +0200

    Removes comment blocks

--- archivo/crawling/ArchivoVersion.py
+++ archivo/crawling/ArchivoVersion.py
@@ -149,46 +149,6 @@ class ArchivoVersion:
         self.__generate_documentation_files()
 
 
-        # checks consistency with and without imports
-        self.is_consistent, output = self.test_suite.getConsistency(
-            raw_file_path + "_type=parsed.ttl", ignoreImports=False
-        )
-        self.is_consistent_noimports, outputNoImports = self.test_suite.getConsistency(
-            raw_file_path + "_type=parsed.ttl", ignoreImports=True
-        )
-        with open(
-                raw_file_path + "_type=pelletConsistency_imports=FULL.txt", "w+"
-        ) as consistencyReport:
-            print(output, file=consistencyReport)
-        with open(
-                raw_file_path + "_type=pelletConsistency_imports=NONE.txt", "w+"
-        ) as consistencyReportNoImports:
-            print(outputNoImports, file=consistencyReportNoImports)
-        # print pellet info files
-        with open(
-                raw_file_path + "_type=pelletInfo_imports=FULL.txt", "w+"
-        ) as pelletInfoFile:
-            print(
-                self.test_suite.getPelletInfo(
-                    raw_file_path + "_type=parsed.ttl", ignoreImports=False
-                ),
-                file=pelletInfoFile,
-            )
-        with open(
-                raw_file_path + "_type=pelletInfo_imports=NONE.txt", "w+"
-        ) as pelletInfoFileNoImports:
-            print(
-                self.test_suite.getPelletInfo(
-                    raw_file_path + "_type=parsed.ttl", ignoreImports=True
-                ),
-                file=pelletInfoFileNoImports,
-            )
-        # profile check for ontology
-        stdout, stderr = self.test_suite.getProfileCheck(
-            raw_file_path + "_type=parsed.ttl"
-        )
-        with open(raw_file_path + "_type=profile.txt", "w+") as profileCheckFile:
-            print(stderr + "\n" + stdout, file=profileCheckFile)
 
         # write the metadata json file
         ontoFiles.altWriteVocabInformation(
@@ -212,21 +172,6 @@ class ArchivoVersion:
             semVersion=self.semantic_version,
             snapshot_url=self.location_uri,
         )
-        # generate lode docu
-        docustring, lode_error = feature_plugins.getLodeDocuFile(
-            self.location_uri, logger=self.logger
-        )
-        if docustring is not None:
-            with open(raw_file_path + "_type=generatedDocu.html", "w+") as docufile:
-                print(docustring, file=docufile)
-
-        # generate pylode docu
-        # pylode_doc = feature_plugins.get_pyLODE_doc_string(
-        #     raw_file_path + "_type=parsed.ttl", self.logger
-        # )
-        # if pylode_doc is not None:
-        #     with open(raw_file_path + "_type=pyLodeDoc.html", "w+") as docufile:
-        #         print(pylode_doc, file=docufile)
 
     def handleTrackThis(self):
         if self.isDev:
