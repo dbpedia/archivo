4dee112acc83f99d66e30cf5b1acf05bb9a62c99
Jenifer Tabita Ciuciu-Kiss <jenifer.tabita.ciuciu.kiss@gmail.com>
2024-08-26 03:42:34 +0200

    remore prints

--- archivo/querying/query_databus.py
+++ archivo/querying/query_databus.py
@@ -120,8 +120,6 @@ def find_previous_version(versions, target_file, target_version):
     previous_version = None
     
     for version_dict in versions:
-        print(target_file)
-        print(version_dict['file']['value'])
         if target_file in version_dict['file']['value']:
             version_str = version_dict['version']['value']
             version_datetime = datetime.strptime(version_str, '%Y.%m.%d-%H%M%S')
@@ -138,13 +136,10 @@ def find_closest_version(versions, target_file, target_version):
     min_diff = float('inf')
     
     for version_dict in versions:
-        print('Target file: ', target_file)
-        print('Version dict: ', version_dict['file']['value'])
         version_str = version_dict['version']['value']
         version_datetime = datetime.strptime(version_str, '%Y.%m.%d-%H%M%S')
         diff = abs((version_datetime - target_datetime).total_seconds())
 
-        print(f'Diff: {diff}')
         if diff == 0.0:
             return version_str
 
